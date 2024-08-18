d0840fc4b8d8cbbdc5f7721561dbf4d200abddb6
Spacemonk <sreeragsathyan2001@gmail.com>
2024-08-18 20:02:57 +0530

    2nd commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -221,23 +221,7 @@ function ProductList() {
            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
          }));
       };
-    {plantsArray.map((category, index) => (
-        <div key={index}>
-            <h1><div>{category.category}</div></h1>
-            <div className="product-list">
-                {category.plants.map((plant, plantIndex) => (
-                <div className="product-card" key={plantIndex}>
-                    <img className="product-image" src={plant.image} alt={plant.name} />
-                    <div className="product-title">{plant.name}</div>
-                    <div className='product-description'>{plant.description}</div>
-                    <div className='product-cost'>{plant.cost}</div>
-                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
-                </div>
-                ))}
-            </div>
-        </div>
-        ))}
-   const styleObj={
+    const styleObj={
     backgroundColor: '#4CAF50',
     color: '#fff!important',
     padding: '15px',
@@ -293,7 +277,21 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category, index) => (
+            <div key={index}>
+            <h1><div>{category.category}</div></h1>
+            <div className="product-list">
+                {category.plants.map((plant, plantIndex) => (
+                <div className="product-card" key={plantIndex}>
+                    <img className="product-image" src={plant.image} alt={plant.name} />
+                    <div className="product-title">{plant.name}</div>
+                    {/*Similarly like the above plant.name show other details like description and cost*/}
+                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                </div>
+                ))}
+            </div>
+            </div>
+            ))}
 
         </div>
  ) :  (
